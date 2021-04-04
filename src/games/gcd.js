88307f5af7ea8e4e0139bac3b6db8743d8150c4b
FmDM78 <FmDm78@gmail.com>
2021-04-04 21:11:33 +0300

    refactoring 04_04_21

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,17 +2,17 @@ import getRandomNum from '../utils.js';
 
 export const taskDescription = 'Find the greatest common divisor of given numbers.';
 
-function getGCD(a, b) {
+function gcd(a, b) {
   if (b === 0) {
     return a;
   }
-  return getGCD(b, a % b);
+  return gcd(b, a % b);
 }
 
 export function getRoundData() {
   const num1 = getRandomNum(100, 1);
   const num2 = getRandomNum(100, 1);
   const question = `${num1} ${num2}`;
-  const answer = String(getGCD(num1, num2));
+  const answer = String(gcd(num1, num2));
   return { question, answer };
 }
--- src/games/progression.js
+++ src/games/progression.js
@@ -4,24 +4,18 @@ export const taskDescription = 'What number is missing in the progression?';
 
 const progressionLength = 10;
 
-function getQuestion(progression, hiddenNumberIndex) {
-  const inputStr = [];
-  for (let i = 0; i < progression.length; i += 1) {
-    if (i !== hiddenNumberIndex) {
-      inputStr.push(progression[i]);
-    } else {
-      inputStr.push('..');
-    }
-  }
-  return inputStr.join(' ');
+function getQuestion(progression, hiddenElementIndex) {
+  const projressionWithHiddenElement = [...progression];
+  projressionWithHiddenElement[hiddenElementIndex] = '..';
+  return projressionWithHiddenElement.join(' ');
 }
 
 export function getRoundData() {
   const start = getRandomNum(10, 1);
   const step = getRandomNum(10, 1);
-  const hiddenNumberIndex = getRandomNum(progressionLength, 0);
+  const hiddenElementIndex = getRandomNum(progressionLength, 0);
   const progression = Array.from({ length: progressionLength }, (_, i) => start + (i * step));
-  const question = getQuestion(progression, hiddenNumberIndex);
-  const answer = String(progression[hiddenNumberIndex]);
+  const question = getQuestion(progression, hiddenElementIndex);
+  const answer = String(progression[hiddenElementIndex]);
   return { question, answer };
 }
